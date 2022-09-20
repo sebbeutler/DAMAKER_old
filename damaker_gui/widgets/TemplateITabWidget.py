328df3b5c331877f33a72bdd8778da07b505a82f
subski <seb.beutler@netcourrier.com>
2022-09-20 05:45:09 +0200

    Template file for new tabs (to add to the docs)

---
+++ damaker_gui/widgets/TemplateITabWidget.py
@@ -0,0 +1,19 @@
+from PySide2.QtWidgets import QFrame, QLabel, QPushButton, QSizePolicy
+from PySide2.QtCore import Qt, Signal
+
+import damaker as dmk
+import damaker_gui as dmk_gui
+import damaker_gui.widgets as widgets
+
+from damaker_gui.widgets.QFrameLayout import *
+
+class ColorAdjustWidget(QFrameLayout, widgets.ITabWidget):
+    name: str = "Template widget"
+    icon: str = u":/flat-icons/icons/flat-icons/timeline.svg"
+    
+    @property
+    def toolbar(self) -> list[widgets.ActionButton]:        
+        return [widgets.ActionButton(self.clearGraph, "Clear", u":/flat-icons/icons/flat-icons/cube.png")]
+    
+    def __init__(self, parent=None):
+        super().__init__(parent, LayoutTypes.Vertical, spacing=5, margin=0)
